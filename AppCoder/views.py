d252e54c39e895ece8b92d662ed3f4c21b92e70a
Matias Escobar <matiasescobar@yahoo.com.ar>
2023-03-02 17:20:37 -0300

    cuarta actualizacion

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from AppCoder.models import Curso
 
-# Create your views here.
-def curso(self):
-    curso= Curso (nombre="Desarrollo WEB", camada= 19881)
-    curso.save()
-    documentoDetexto=f"----> Curso: {curso.nombre} Camada: {curso.camada}"
+def inicio (request):
+    return render (request, "inicio.html")
 
-    return HttpResponse (documentoDetexto)
+def cursos (request):
+   return render (request, "cursos.html")
+
+def profesores (request):
+    return render (request, "profesores.html")
+
+def estudiantes (request):
+    return render (request, "estudiantes.html")
+
+def entregables (request):
+    return render (request, "entregables.html")
